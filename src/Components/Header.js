f5b24fd44141d66fd6c97d1b10b8ea365603c4f1
Victor Ciavarella <victor.ciavarella@chasacademy.se>
2019-03-13 17:11:22 +0100

    changed button to href link

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,15 +1,17 @@
-import React from 'react'
-import P from './P'
-import H3 from './H3'
-import Button from './Button'
-import FlexContainer from './FlexContainer'
+import React from 'react';
+import P from './P';
+import H3 from './H3';
+import Button from './Button';
+import FlexContainer from './FlexContainer';
 
 const Header = props => (
-  <FlexContainer flex="header">
-    <H3 text="Music Per Minute" />
-    <P text="About" />
-    <Button onClick="login" text="Sign in with Spotify" />
+  <FlexContainer flex='header'>
+    <H3 text='Music Per Minute' />
+    <P text='About' />
+    <a href='https://mpm-node-backend.herokuapp.com/auth/'>
+      Sign in with Spotify
+    </a>
   </FlexContainer>
-)
+);
 
-export default Header
+export default Header;
