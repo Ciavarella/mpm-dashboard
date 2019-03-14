20215b20e771b2f9af3e8a250dd17bc6dd5e5939
Victor Ciavarella <victor.ciavarella@chasacademy.se>
2019-03-14 12:54:59 +0100

    added styles and new view

--- src/Views/AuthLogin.js
+++ src/Views/AuthLogin.js
@@ -1,12 +1,21 @@
-import React from 'react';
-import '../Styles/AuthLogin.css';
+import React from 'react'
+import '../Styles/AuthLogin.css'
+import '../Styles/AuthRedirect.css'
+import '../index.css'
+import spotifyIcon from '../Assets/spotifyIcon.png'
 
 const AuthLogin = () => {
   return (
-    <div>
-      <p>hello</p>
+    <div className="mpm-helper-container">
+      <div className="spotifyContainer">
+        <img className="spotifyLogo" alt="SpotifyLogo" src={spotifyIcon} />
+        <h2>Music Per Minute</h2>
+        <a href="https://mpm-node-backend.herokuapp.com/auth/" className="btn">
+          Login with Spotify
+        </a>
+      </div>
     </div>
-  );
-};
+  )
+}
 
-export default AuthLogin;
+export default AuthLogin
--- src/Views/AuthRedirect.js
+++ src/Views/AuthRedirect.js
@@ -1,36 +1,37 @@
-import React, { useRef } from 'react';
-import spotifyIcon from '../Assets/spotifyIcon.png';
-import { getParams } from '../Utils/helpers';
-import '../Styles/AuthRedirect.css';
+import React, { useRef } from 'react'
+import spotifyIcon from '../Assets/spotifyIcon.png'
+import '../Styles/AuthRedirect.css'
 
+/**
+ * Takes the url and slices it to get tokens.
+ */
 const AuthRedirect = () => {
-  const textInputRef = useRef(null);
-  const params = getParams(window.location);
+  const textInputRef = useRef(null)
+  const params = window.location.search
+  const token = params.slice(14)
 
+  /**
+   * Copy the token to clipboard.
+   */
   const copyToken = e => {
-    textInputRef.current.select();
-    document.execCommand('copy');
-    e.target.focus();
-  };
+    textInputRef.current.select()
+    document.execCommand('copy')
+    e.target.focus()
+  }
 
   return (
-    <div className='mpm-helper-container'>
-      <img className='spotifyLogo' src={spotifyIcon} />
-      {params && params.access_token && (
-        <div className='tokenContainer'>
-          <input
-            className='token'
-            ref={textInputRef}
-            value={params.access_token}
-            readOnly
-          />
-          <button className='btn' onClick={copyToken}>
+    <div className="mpm-helper-container">
+      <img className="spotifyLogo" alt="SpotifyLogo" src={spotifyIcon} />
+      {params && token && (
+        <div className="tokenContainer">
+          <input className="token" ref={textInputRef} value={token} readOnly />
+          <button className="btn" onClick={copyToken}>
             Copy to clipboard
           </button>
         </div>
       )}
     </div>
-  );
-};
+  )
+}
 
-export default AuthRedirect;
+export default AuthRedirect
