362ebb04e2acb4701e67f91979db9b53aa87a820
Victor Ciavarella <victor.ciavarella@chasacademy.se>
2019-04-12 13:09:08 +0200

    Added about page

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import NotFound from './Views/NotFound.js'
 import { AuthProvider } from './Auth/AuthContext'
 import PrivateRoute from './Auth/PrivateRoute'
 import Settings from './Views/Dashboard/Settings'
+import About from './Views/About'
 
 class App extends Component {
   render() {
@@ -26,6 +27,7 @@ class App extends Component {
             <Route exact path="/auth/login" component={AuthLogin} />
             <PrivateRoute path="/dashboard" component={Dashboard} />
             <PrivateRoute exact path="/settings" component={Settings} />
+            <Route path="/about" component={About} />
             <Route path="/*" component={NotFound} />
           </Switch>
         </Router>
